727e91c4fa9ca58f8b6a40f92e0a000dcc05454f
Anthony Tarbinian <atar137h@gmail.com>
2021-06-18 17:47:29 -0700

    Moved time function to a goroutine

--- src/main.go
+++ src/main.go
@@ -39,20 +39,67 @@ type configInterface struct{
 }
 
 
+/*
+goroutines:
+- one for reading config
+- one for each module 
 
+all the channels return to the main function where its all put together
+*/
 func main()  {
 
+	var configLastModified time.Time 
+
     var config configInterface
-	parsedConfig := config.retrieveConfig()
+	parsedConfig := config.retrieveConfig(&configLastModified)
 
 	fmt.Println("Config file successfully loaded")
-	fmt.Println(parsedConfig)
+	fmt.Println(*parsedConfig)
+
+
 
 	loopCounter := 0
 
+	/*
+	Retrieve config and get modules
+	goroutine for config refresh with a loop that sleeps at whatever the refresh rate says
+	goroutine all the modules where each function its own loop with a channel
+	another for loop the end that loops every 1 second to update the statusbar
+	*/
+	modules := parsedConfig.Modules
+
+	timeChan := make(chan string)
+	// dateChan := make(chan string)
+	// cpuChan := make(chan string)
+	// batteryChan := make(chan string)
+	// ramChan := make(chan string)
+	// brightnessChan := make(chan string)
+	// pulseChan := make(chan string)
+
+	for _, module := range modules {
+		switch module {
+			case "time":
+				go GetTime(timeChan, parsedConfig)
+			case "date":
+
+			case "mpris":
+
+			case "cpu":
+
+			case "battery":
+
+			case "ram":
+				
+			case "brightness":
+
+			case "pulse":
+			
+		}
+	}
+
 	for {
 		loopCounter++
-		modules := parsedConfig.Modules
+		// modules := parsedConfig.Modules
 
 		configRefreshRate, err := strconv.Atoi(parsedConfig.RefreshConfigRate)
 		if err != nil {
@@ -61,48 +108,52 @@ func main()  {
 
 
 		output := ""
-		for idx, module := range modules {
-			moduleData := ""
-			switch module {
-				case "time":
-					moduleData += GetTime(parsedConfig.TimeFormat, 
-											parsedConfig.TwentyFourHour)
-				case "date":
-					moduleData += GetDate(parsedConfig.DateFormat)
-				case "mpris":
-					moduleData += GetMpris(parsedConfig.PlayingFormat, 
-											parsedConfig.PausedFormat, 
-											parsedConfig.MprisMaxLength, 
-											parsedConfig.ScrollMpris,
-											parsedConfig.MprisScrollSpeed)
-				case "cpu":
-					moduleData += GetCPUTemp(parsedConfig.CPUTempUnits)
-					// moduleData += F.GetCPUUsage()
-				case "battery":
-					moduleData += GetBatteryPercentage(parsedConfig.BatteryFormat)
-				case "ram":
-					if parsedConfig.RAMDisplay == "Percentage" {
-						moduleData += GetRAMUsage("format placeholder", false)
-					} else if parsedConfig.RAMDisplay == "Raw" {
-						moduleData += GetRAMData(parsedConfig.RAMRawFormat, 
-													parsedConfig.RAMRawUnit)
-					}
-				case "brightness":
-					moduleData += GetBrightness()
-				case "pulse":
-					moduleData += GetPulseVolume(parsedConfig.PulseMutedFormat, 
-													parsedConfig.PulseVolumeFormat)
-			}
-			if moduleData == "" {
-				continue
-			}
+		// for idx, module := range modules {
+		// 	moduleData := ""
+		// 	switch module {
+		// 		case "time":
+		// 			// moduleData += GetTime(parsedConfig.TimeFormat, 
+		// 			// 						parsedConfig.TwentyFourHour)
+		// 			go GetTime(timeChan, parsedConfig)
+		// 			moduleData += <-timeChan
+		// 		case "date":
+		// 			moduleData += GetDate(parsedConfig.DateFormat)
+		// 		case "mpris":
+		// 			moduleData += GetMpris(parsedConfig.PlayingFormat, 
+		// 									parsedConfig.PausedFormat, 
+		// 									parsedConfig.MprisMaxLength, 
+		// 									parsedConfig.ScrollMpris,
+		// 									parsedConfig.MprisScrollSpeed)
+		// 		case "cpu":
+		// 			moduleData += GetCPUTemp(parsedConfig.CPUTempUnits)
+		// 			// moduleData += F.GetCPUUsage()
+		// 		case "battery":
+		// 			moduleData += GetBatteryPercentage(parsedConfig.BatteryFormat)
+		// 		case "ram":
+		// 			if parsedConfig.RAMDisplay == "Percentage" {
+		// 				moduleData += GetRAMUsage("format placeholder", false)
+		// 			} else if parsedConfig.RAMDisplay == "Raw" {
+		// 				moduleData += GetRAMData(parsedConfig.RAMRawFormat, 
+		// 											parsedConfig.RAMRawUnit)
+		// 			}
+		// 		case "brightness":
+		// 			moduleData += GetBrightness()
+		// 		case "pulse":
+		// 			moduleData += GetPulseVolume(parsedConfig.PulseMutedFormat, 
+		// 											parsedConfig.PulseVolumeFormat)
+		// 	}
+		// 	if moduleData == "" {
+		// 		continue
+		// 	}
 
-			if idx != len(modules) - 1 {
-				moduleData += parsedConfig.ModuleSeperator
-			}
+		// 	if idx != len(modules) - 1 {
+		// 		moduleData += parsedConfig.ModuleSeperator
+		// 	}
 
-			output += moduleData
-		}
+		// 	output += moduleData
+		// }
+
+		output += <-timeChan
 
 
 		xsetroot := "xsetroot"
@@ -117,19 +168,34 @@ func main()  {
 			return 
 		}
 		if parsedConfig.RefreshConfig && loopCounter % configRefreshRate == 0 {
-			parsedConfigChan := make(chan configInterface)	
-			go refreshConfig(parsedConfigChan, parsedConfig) 
+			parsedConfigChan := make(chan *configInterface)	
+			go refreshConfig(parsedConfigChan, *parsedConfig, &configLastModified) 
 			parsedConfig = <- parsedConfigChan
 			fmt.Println(parsedConfig)
 		}
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(time.Second)
 	}
 
 }
 
 
 
-func (config *configInterface) retrieveConfig() configInterface {
+func (config *configInterface) retrieveConfig(configLastModified *time.Time) *configInterface {
+	stats, err := os.Stat(fmt.Sprintf("%s/.config/go-dwm-statusbar/config.yaml", os.Getenv("HOME")))
+	if err != nil {
+		fmt.Println("Error with reading config file")
+	}
+	newModTime := stats.ModTime()
+	if newModTime.IsZero() {
+		*configLastModified = newModTime
+	} else {
+		if newModTime.After(*configLastModified) {
+			*configLastModified = newModTime
+		} else {
+			fmt.Println("Config not modified")
+			return config
+		}
+	}
 	data, err := ioutil.ReadFile(fmt.Sprintf("%s/.config/go-dwm-statusbar/config.yaml", os.Getenv("HOME")))
 
 	fmt.Println("Updating config")
@@ -141,9 +207,9 @@ func (config *configInterface) retrieveConfig() configInterface {
 
 	data = nil
 	
-	return *config
+	return config
 }
 
-func refreshConfig(config chan configInterface, configStruct configInterface) {
-		config <- configStruct.retrieveConfig()
+func refreshConfig(config chan *configInterface, configStruct configInterface,  configLastModified *time.Time) {
+		config <- configStruct.retrieveConfig(configLastModified)
 }
--- src/time.go
+++ src/time.go
@@ -10,28 +10,37 @@ const defaultFormat = "03:04:05"
 const default24Format = "15:04:05"
 
 //GetTime retrieves the current time and formats it according to the user's configuration
-func GetTime(format string, twentyFourHour bool) string{
-	fullTime := time.Now()
+func GetTime(timeChan chan string, config *configInterface) {
+	for {
+		fullTime := time.Now()
+		format := config.TimeFormat
+		twentyFourHour := config.TwentyFourHour
 
-	if format == "" {
-		if twentyFourHour {
-			return fullTime.Format(default24Format)
+		if format == "" {
+			if twentyFourHour {
+				timeChan <- fullTime.Format(default24Format)
+			} else {
+				timeChan <- fullTime.Format(defaultFormat)
+			}
+			time.Sleep(time.Second)
+			continue	
 		}
-			return fullTime.Format(defaultFormat)
-	}
 
-	format = strings.ReplaceAll(format, "MM", "04")
-	format = strings.ReplaceAll(format, "SS", "05")
+		format = strings.ReplaceAll(format, "MM", "04")
+		format = strings.ReplaceAll(format, "SS", "05")
 
-	var formattedTime string
-	if twentyFourHour {
-		format = strings.ReplaceAll(format, "HH", "15")
-		formattedTime = fullTime.Format(format)
-	} else {
-		format = strings.ReplaceAll(format, "HH", "03")
-		formattedTime = fullTime.Format(format)
+		var formattedTime string
+		if twentyFourHour {
+			format = strings.ReplaceAll(format, "HH", "15")
+			formattedTime = fullTime.Format(format)
+		} else {
+			format = strings.ReplaceAll(format, "HH", "03")
+			formattedTime = fullTime.Format(format)
+		}
+		// return formattedTime
+		timeChan <- formattedTime
+		time.Sleep(time.Second)
 	}
-	return formattedTime
 }
 
 
